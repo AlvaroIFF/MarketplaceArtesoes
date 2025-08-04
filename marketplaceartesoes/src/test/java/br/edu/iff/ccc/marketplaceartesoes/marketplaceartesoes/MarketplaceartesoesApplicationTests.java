c980652d63f16075b94fb5c4e1cd235da380fdcc
AlvaroRFP <alvaropinheirorfp@outlook.com>
2025-08-04 15:14:24 -0300

    primeiro commit maven

---
+++ marketplaceartesoes/src/test/java/br/edu/iff/ccc/marketplaceartesoes/marketplaceartesoes/MarketplaceartesoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.marketplaceartesoes.marketplaceartesoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceartesoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
